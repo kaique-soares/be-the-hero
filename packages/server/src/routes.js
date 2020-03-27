811ef3b97af42e117a04ad2e9162248f81858f7c
Kaique Soares <kaiquesoares.dev@gmail.com>
2020-03-27 17:27:23 -0300

    fix: corrects validation of whatsapp field in the creation of an ONG

--- packages/server/src/routes.js
+++ packages/server/src/routes.js
@@ -20,7 +20,7 @@ routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    whatsapp: Joi.number().min(10).max(11).required(),
+    whatsapp: Joi.string().min(10).max(11).required(),
     city: Joi.string().required(),
     uf: Joi.string().length(2),
   }),
