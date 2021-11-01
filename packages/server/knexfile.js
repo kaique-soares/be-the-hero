1f73875cf3c2c9819189d314d276a16bebe2fa10
Kaique Soares <kaiquesoares.dev@gmail.com>
2021-11-01 15:33:10 -0300

    chore: set up server into monorepo structure

--- packages/server/knexfile.js
+++ packages/server/knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/db.sqlite'
+      filename: './src/database/be-the-hero.sqlite'
     },
     migrations: {
       directory: './src/database/migrations'
