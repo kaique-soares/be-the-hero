5adb74289b68df308dca41b50b1b9401cfc26304
Kaique Soares <kaiquesoares.dev@gmail.com>
2022-02-19 15:14:34 -0300

    chore: move server folder to project root folder

---
+++ server/src/database/connection.js
@@ -0,0 +1,11 @@
+const knex = require('knex');
+
+const configuration = require('../../knexfile');
+
+const envConfig = process.env.NODE_ENV === 'test' 
+  ? configuration.test 
+  : configuration.development;
+
+const connection = knex(envConfig);
+
+module.exports = connection;
